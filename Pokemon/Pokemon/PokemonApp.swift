b22d42789403fe303d5108973a77e6d94310a821
ValentinaKry <98461511+ValentinaKry@users.noreply.github.com>
2022-10-29 11:44:35 +0300

    Add files via upload

---
+++ Pokemon/Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Valentina Krysak on 29.10.22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
